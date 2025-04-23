268967fdf60816b7e57128b7328435107f2d9a02
gokul-vsi <gokulnath2923@gmail.com>
2025-04-23 15:56:02 +0530

    step-2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import header from '../images/header.png'
 import home2 from '../images/home2.png'
+import home3 from '../images/home3.png'
 import logo1 from '../images/logo1.svg'
 import logo2 from '../images/logo2.svg'
 import logo3 from '../images/logo3.svg'
 import logo4 from '../images/logo4.svg'
 import logo5 from '../images/logo5.svg'
 import logo6 from '../images/logo6.svg'
+import doc1 from '../doctors/doc1'
 
 export const Home = () => {
   return (
     <div>
         {/* header */}
-        <div className="container mx-auto mt-5 md:mt-16 mb-5 rounded bg-blue-500">
+        <div className="container mx-auto mt-5 md:mt-16 mb-10 rounded bg-blue-500">
             <div className="flex flex-col  md:flex-row">
             <div className="md:w-1/2  text-center md:text-start   md:ms-14  ">
               <h1 className='md:text-5xl text-2xl text-white font-medium  md:mt-24 '>Book Appointment
@@ -69,6 +71,49 @@ arrow_forward
                 </div>
             </div>
             </div>
+        </div>
+        {/* ================================================= */}
+        {/* Doctors List */}
+        <h1 className='text-center mt-10 text-3xl font-medium mb-3 '>
+        Top Doctors to Book
+        </h1>
+        <p className='text-center text-sm '>
+        Simply browse through our extensive list of trusted doctors.
+        </p>
+        <div className="container mx-auto p-5">
+        <div  className='grid grid-cols-2 md:grid-cols-4 gap-4'>
+          {
+            doc1.map((dd,index)=>(
+               <div key={index} className=' border overflow-hidden border-blue-200 rounded-xl hover:translate-y-[-10px] transition-all duration-500  '>
+                   <img src={dd.image} className='w-full h-auto bg-gray-100  ' alt="" />
+                   <div className="flex justify-center items-center flex-row gap-2 mt-2 ">
+                    <p className='w-2 h-2 rounded-full bg-green-500'></p>
+                    <p className='text-green-500'>Available</p>
+                   </div>
+                      <p className='text-center font-medium ' >{dd.name}</p>
+                      <p className='text-center mb-2'>{dd.role}</p>  
+               </div>
+            ))
+          }
+        </div>
+        </div>
+        {/* ===================================================== */}
+        {/* footer-one */}
+        <div className="container mx-auto p-5">
+          <div className="flex flex-row rounded-lg bg-blue-500 ">
+            <div className="md:w-1/2 md:ms-14 ms-1.5" >
+              <h1 className='md:text-5xl text-2x text-white font-medium mx-1 text-justify mt-10 md:mt-40 '>
+              Book Appointment
+With 100+ Trusted Doctors
+              </h1>
+              <button className='md:px-4 md:py-2 px-1 py-1.5 rounded-4xl md:mt-8 mt-4 bg-white text-gray-400 font-bold hover:scale-110 transition-all duration-500 '>Create Account</button>
+            </div>
+            <div className="md:w-1/2">
+               <div className="flex justify-center items-center">
+               <img src={home3} className='md:w-96 md:py-2 ' alt="" />
+               </div>
+            </div>
+          </div>
         </div>
     </div>
   )
---
+++ src/doctors/doc1.js
@@ -0,0 +1,73 @@
+import doc1 from "../images/doc1.png";
+import doc2 from "../images/doc2.png";
+import doc3 from "../images/doc3.png";
+import doc4 from "../images/doc4.png";
+import doc5 from "../images/doc5.png";
+import doc6 from "../images/doc6.png";
+import doc7 from "../images/doc7.png";
+import doc8 from "../images/doc8.png";
+import doc9 from "../images/doc9.png";
+import doc10 from "../images/doc10.png";
+const doclist = [
+  {
+    id: 1,
+    name: "Dr.Richards James",
+    image: doc1,
+    role:"General physician"
+  },
+  {
+    id: 2,
+    name: "Dr.Emily Larson",
+    image: doc2,
+    role:"Gynecologist"
+  },
+  {
+    id: 1,
+    name: "Dr.Maxwell",
+    image: doc3,
+    role:"Dermatologist"
+  },
+  {
+    id: 1,
+    name: "Dr.Chistopher Lee",
+    image: doc4,
+    role:"Pediatricians"
+  },
+  {
+    id: 1,
+    name: "Dr.Jennifer Garcia",
+    image: doc5,
+    role:"Neurologist"
+  },
+  {
+    id: 1,
+    name: "Dr.Andrew willams",
+    image: doc6,
+    role:"Gastroenterologist"
+  },
+  {
+    id: 1,
+    name: "Dr.christopher davis",
+    image: doc7,
+    role:"General physician"
+  },
+  {
+    id: 1,
+    name: "Dr.Timothy white",
+    image: doc8,
+    role:"Gynecologist"
+  },
+  {
+    id: 1,
+    name: "Dr.Ava mitchell",
+    image: doc9,
+    role:"Dermatologist"
+  },
+  {
+    id: 1,
+    name: "Dr.jeffrey king",
+    image: doc10,
+    role:"Pediatricians"
+  },
+];
+export default doclist;
