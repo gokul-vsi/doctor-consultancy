609219d4cf79f647c007456eb1484bd8262544bb
gokul-vsi <gokulnath2923@gmail.com>
2025-04-24 17:16:21 +0530

    step3

---
+++ src/doctors/doc2.js
@@ -0,0 +1,109 @@
+import doc1 from "../images/doc1.png";
+import doc2 from "../images/doc2.png";
+import doc3 from "../images/doc3.png";
+import doc4 from "../images/doc4.png";
+import doc5 from "../images/doc5.png";
+import doc6 from "../images/doc6.png";
+import doc7 from "../images/doc7.png";
+import doc8 from "../images/doc8.png";
+import doc9 from "../images/doc9.png";
+import doc10 from "../images/doc10.png";
+import doc11 from "../images/doc11.png";
+import doc12 from "../images/doc12.png";
+import doc13 from "../images/doc13.png";
+import doc14 from "../images/doc14.png";
+import doc15 from "../images/doc15.png";
+
+const doc2list = [
+  {
+    id: 1,
+    name: "Dr.Richards James",
+    image: doc1,
+    role:"General physician"
+  },
+  {
+    id: 2,
+    name: "Dr.Emily Larson",
+    image: doc2,
+    role:"Gynecologist"
+  },
+  {
+    id: 1,
+    name: "Dr.Maxwell",
+    image: doc3,
+    role:"Dermatologist"
+  },
+  {
+    id: 1,
+    name: "Dr.Chistopher Lee",
+    image: doc4,
+    role:"Pediatricians"
+  },
+  {
+    id: 1,
+    name: "Dr.Jennifer Garcia",
+    image: doc5,
+    role:"Neurologist"
+  },
+  {
+    id: 1,
+    name: "Dr.Andrew willams",
+    image: doc6,
+    role:"Gastroenterologist"
+  },
+  {
+    id: 1,
+    name: "Dr.christopher davis",
+    image: doc7,
+    role:"General physician"
+  },
+  {
+    id: 1,
+    name: "Dr.Timothy white",
+    image: doc8,
+    role:"Gynecologist"
+  },
+  {
+    id: 1,
+    name: "Dr.Ava mitchell",
+    image: doc9,
+    role:"Dermatologist"
+  },
+  {
+    id: 1,
+    name: "Dr.jeffrey king",
+    image: doc10,
+    role:"Pediatricians"
+  },
+  {
+    id: 1,
+    name: "Dr. Zoe Kelly",
+    image: doc11,
+    role:" Neurologist"
+  },
+  {
+    id: 1,
+    name: "Dr. Patrick Harris",
+    image: doc12,
+    role:"Gastroenterologist"
+  },
+  {
+    id: 1,
+    name: "Dr. Chloe Evans",
+    image: doc13,
+    role:" General physician"
+  },
+  {
+    id: 1,
+    name: "Dr. Ryan Martinez",
+    image: doc14,
+    role:"Gynecologist"
+  },
+  {
+    id: 1,
+    name: "Dr. Amelia Hill",
+    image: doc15,
+    role:"Pediatricians"
+  },
+];
+export default doc2list;
